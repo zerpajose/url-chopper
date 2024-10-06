5d2abd261def5b252eb352e55aff46da120f95c3
Jose Zerpa <zjose88@gmail.com>
2024-10-06 17:37:44 -0300

    Validations

---
+++ src/validations/url-service.shemas.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createShortUrlSchema = z.object({
+  url: z.string().url(),
+});
+
+export const updateShortUrlSchema = z.object({
+  url: z.string().url(),
+});
