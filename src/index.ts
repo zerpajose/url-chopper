cae6ae38d1673e29b38814b99f5147c099b7ad63
Jose Zerpa <zjose88@gmail.com>
2024-10-08 00:40:39 -0300

    Refactor PORT assignment to use environment variable

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get('/ping', (_req, res) => {
   console.log(`someone pinged here! ${new Date().toLocaleDateString()}`);
