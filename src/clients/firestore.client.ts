3a49e738b9266932f7ad731f86d01fa1aced38c0
Jose Zerpa <zjose88@gmail.com>
2024-10-08 20:56:08 -0300

    JSDocs for Firestore client

--- src/clients/firestore.client.ts
+++ src/clients/firestore.client.ts
@@ -1,5 +1,10 @@
 import { Firestore } from '@google-cloud/firestore';
 
+/**
+ * Firestore client
+ * @type {Firestore}
+ * @see https://googleapis.dev/nodejs/firestore/latest/Firestore.html
+ */
 export const db = new Firestore({
   projectId: 'fourth-tiger-437722-i4',
 });
