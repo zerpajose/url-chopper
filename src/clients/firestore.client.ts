bfa45af51797fa63b7df61140693f29ef9d338f6
Jose Zerpa <zjose88@gmail.com>
2024-10-08 22:17:26 -0300

    Refactor Firestore client to use environment variable for project ID

--- src/clients/firestore.client.ts
+++ src/clients/firestore.client.ts
@@ -1,10 +1,11 @@
 import { Firestore } from '@google-cloud/firestore';
 
+
 /**
  * Firestore client
  * @type {Firestore}
  * @see https://googleapis.dev/nodejs/firestore/latest/Firestore.html
  */
 export const db = new Firestore({
-  projectId: 'fourth-tiger-437722-i4',
+  projectId: process.env.GOOGLE_PROJECT_ID || 'fourth-tiger-437722-i4',
 });
